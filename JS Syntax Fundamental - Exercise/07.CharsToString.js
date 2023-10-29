ea3bbf733822b48057564a8ed37ce010ce12af1c
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-10-29 21:20:35 +0200

    JS Syntax Fundamental - Exercise

---
+++ JS Syntax Fundamental - Exercise/07.CharsToString.js
@@ -0,0 +1,7 @@
+function charsToString(char1, char2, char3) {
+    let finalString = '';
+    finalString += char1;
+    finalString += char2;
+    finalString += char3;
+    console.log(finalString);
+}
