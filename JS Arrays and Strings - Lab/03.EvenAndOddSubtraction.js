37a303201709a36614e79cab72cc9bd33f1fa6ed
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-11-21 21:35:59 +0200

    JS Arrays and Strings - Lab

---
+++ JS Arrays and Strings - Lab/03.EvenAndOddSubtraction.js
@@ -0,0 +1,15 @@
+function subtraction(arr) {
+    let evenSum = 0;
+    let oddSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = Number(arr[i]);
+        if (arr[i] % 2 == 0) {
+            evenSum += arr[i];
+        } else {
+            oddSum += arr[i];
+        }
+    }
+
+    let diff = evenSum - oddSum;
+    console.log(diff);
+}
