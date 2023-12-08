2a4ea2c904b0f078f32071c5e3fdc2249196fe0f
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-08 20:13:30 +0200

    JS Arrays and Strings - Exercise

---
+++ JS Arrays and Strings - Exercise/8.Pascal-CaseSplitter.js
@@ -0,0 +1,7 @@
+function pascalCase(text) {
+    let wordsRegex = /[A-Z][a-z]*/g;
+    let wordsArray = text.match(wordsRegex);
+    let resultString = wordsArray.join(', ');
+
+    console.log(resultString);
+}
