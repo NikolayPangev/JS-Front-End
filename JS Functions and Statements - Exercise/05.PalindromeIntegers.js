24d7be89d03d868936386c71356e26e29af48c92
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-09 19:29:11 +0200

    JS Functions and Statements - Exercise

---
+++ JS Functions and Statements - Exercise/05.PalindromeIntegers.js
@@ -0,0 +1,13 @@
+function palindromeIntegers(arr) {
+
+    let num;
+    function isPalindrome(num) {
+        let numAsString = num.toString();
+        let reversedNum = numAsString.split('').reverse().join('');
+        return numAsString === reversedNum;
+    }
+
+    for (const currentNumber of arr) {
+        console.log(isPalindrome(currentNumber));
+    }
+}
