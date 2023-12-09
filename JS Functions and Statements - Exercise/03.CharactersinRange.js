de8cadff0ee0882c3d525073cb6b323ba8344aad
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-09 17:15:10 +0200

    JS Functions and Statements - Exercise

--- JS Functions and Statements - Exercise/03.CharactersinRange.js
+++ JS Functions and Statements - Exercise/03.CharactersinRange.js
@@ -1,3 +1,12 @@
-function charactersInRange(start, end){
-    
+function charactersInRange(firstChar, secondChar) {
+    let result = '';
+    let start = Math.min(firstChar.charCodeAt(), secondChar.charCodeAt())
+    let end = Math.max(firstChar.charCodeAt(), secondChar.charCodeAt());
+
+    for (let currentCode = ++start; currentCode < end; currentCode++) {
+        const currentChar = String.fromCharCode(currentCode);
+
+        result += `${currentChar} `;
+    }
+    console.log(result);
 }
