de743aee1f6f7c29ebc590399b96fee9128fc85c
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-09 17:00:37 +0200

    JS Functions and Statements - Exercise

---
+++ JS Functions and Statements - Exercise/02.AddAndSubtract.js
@@ -0,0 +1,8 @@
+function addAndSubtract(num1, num2, num3) {
+    let sum = (num1, num2) => {
+        return num1 + num2;
+    }
+
+    let result = sum(num1, num2) - num3;
+    console.log(result);
+}
