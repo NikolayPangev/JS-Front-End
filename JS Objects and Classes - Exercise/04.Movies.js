a1b3c3fbad4103d564d6a221ce334382d092673a
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-12 14:31:24 +0200

    JS Objects and Classes - Exercise

---
+++ JS Objects and Classes - Exercise/04.Movies.js
@@ -0,0 +1,37 @@
+function solveMovies(arrStr) {
+    let movies = [];
+
+    arrStr.forEach(element => {
+        if (element.includes('addMovie')) {
+
+            let movie = element.split('addMovie ')[1];
+
+            movies.push({ name: movie });
+
+        } else if (element.includes('directedBy')) {
+
+            let [movie, director] = element.split(' directedBy ');
+
+            let searching = movies.find((el) =>  el.name === movie )
+
+            if (searching) {
+                searching['director'] = director;
+            }
+        } else if (element.includes('onDate')) {
+
+            let [movie, date] = element.split(' onDate ');
+
+            let searching = movies.find((el) =>  el.name === movie )
+
+            if (searching) {
+                searching['date'] = date;
+            }
+        }
+    });
+
+    movies.forEach((movie) => {
+        if(movie.name && movie.director && movie.date){
+            console.log(JSON.stringify(movie));
+        }
+    });
+}
