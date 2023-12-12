9819379a27ecb2cec88d65f7ed3617c17428aaa6
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-12 17:21:02 +0200

    JS DOM Introduction - Lab

--- JS DOM Introduction - Lab/01.Edit Element/EditElement.js
+++ JS DOM Introduction - Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,5 @@
-function editElement() {
-    // TODO
+function editElement(ref, match, replacer) {
+    const content = ref.textContent;
+    const editedText = content.replace(new RegExp(match, 'g'), replacer)
+    ref.textContent = editedText;
 }
