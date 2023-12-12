1ba8c2f2b19017a82da9db982b5c24965dc186d0
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-12 20:42:57 +0200

    JS DOM Introduction - Exercise

--- JS DOM Introduction - Exercise/01. Subtraction/subtract.js
+++ JS DOM Introduction - Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,14 @@
 function subtract() {
-    console.log('TODO:...');
+    var firstNumberValue = document.getElementById("firstNumber").value;
+    var secondNumberValue = document.getElementById("secondNumber").value;
+
+    var firstNumber = parseFloat(firstNumberValue);
+    var secondNumber = parseFloat(secondNumberValue);
+
+    if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+        var result = firstNumber - secondNumber;
+
+        var resultDiv = document.getElementById("result");
+        resultDiv.textContent = '' + result;
+    }
 }
