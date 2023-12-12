d5b9da2275e7cdd44881390d045e27b39798102c
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-12 21:43:07 +0200

    JS DOM Introduction - Exercise

--- JS DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ JS DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById('text').value;
+  let currentCase = document.getElementById('naming-convention').value;
+  let result = document.getElementById('result');
+
+  if (currentCase === 'Camel Case') {
+    let sentence = [];
+
+    input = input.split(' ');
+
+    sentence.push(input[0].toLowerCase());
+
+    for (let index = 1; index < input.length; index++) {
+      sentence.push(input[index].charAt(0).toUpperCase() + input[index].slice(1).toLowerCase());
+    }
+    result.textContent = sentence.join('');
+
+  } else if (currentCase === 'Pascal Case') {
+    let sentence = [];
+
+    input = input.split(' ');
+
+    for (let index = 0; index < input.length; index++) {
+      sentence.push(input[index].charAt(0).toUpperCase() + input[index].slice(1).toLowerCase());
+    }
+
+    result.textContent = sentence.join('');
+  } else {
+    result.textContent = 'Error!';
+  }
 }
