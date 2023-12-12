42267f868134b038a35a2d9734ad3f3c622308f9
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-12 18:09:21 +0200

    JS DOM Introduction - Lab

--- JS DOM Introduction - Lab/06.Sum Table/SumTable.js
+++ JS DOM Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,14 @@
 function sumTable() {
+    let tableRows = document.querySelectorAll("table tbody tr");
+    let total = 0;
 
+    for (let i = 0; i < tableRows.length; i++) {
+        let cols = tableRows[i].children;
+        let cost = cols[cols.length - 1].textContent.trim();
+
+        if (!isNaN(Number(cost))) {
+            total += Number(cost);
+        }
+    }
+    document.getElementById("sum").textContent = total.toFixed(2);
 }
