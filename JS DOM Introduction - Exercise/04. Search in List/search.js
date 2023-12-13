b6ec8185ae87b78e82741b4e5367e27eeb62d55e
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-13 22:51:55 +0200

    JS DOM Introduction - Exercise

--- JS DOM Introduction - Exercise/04. Search in List/search.js
+++ JS DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,21 @@
 function search() {
-   // TODO
+   let listItems = Array.from(document.querySelectorAll('li'));
+   let searching = document.getElementById('searchText').value;
+   let result = document.getElementById('result');
+   let matches = 0;
+
+   for (const li of listItems) {
+      li.style.fontWeight = '';
+      li.style.textDecoration = '';
+   }
+
+   for (const li of listItems) {
+      if(li.textContent.includes(searching)){
+         matches++;
+         li.style.fontWeight = 'bold';
+         li.style.textDecoration = 'underline';
+      }
+   }
+
+   result.textContent = `${matches} matches found`;
 }
