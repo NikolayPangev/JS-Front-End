198347c808db77474b4cf0bd5038a8eb4d3529dd
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-14 23:07:11 +0200

    JS DOM Manipulation and Events - Exercise

--- JS DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
+++ JS DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,42 @@
 function attachEventsListeners() {
+    let days = document.getElementById('days');
+    let daysButton = document.getElementById('daysBtn');
 
-    console.log('TODO:...');
+    let hours = document.getElementById('hours');
+    let hoursButton = document.getElementById('hoursBtn');
+
+    let minutes = document.getElementById('minutes');
+    let minutesButton = document.getElementById('minutesBtn');
+
+    let seconds = document.getElementById('seconds');
+    let secondsButton = document.getElementById('secondsBtn');
+
+    daysButton.addEventListener('click', daysConvert);
+    hoursButton.addEventListener('click', hoursConvert);
+    minutesButton.addEventListener('click', minutesConvert);
+    secondsButton.addEventListener('click', secondsConvert);
+
+    function daysConvert(params) {
+        hours.value = days.value * 24;
+        minutes.value = hours.value * 60;
+        seconds.value = minutes.value * 60;
+    }
+
+    function hoursConvert(params) {
+        days.value = hours.value / 24;
+        minutes.value = hours.value * 60;
+        seconds.value = minutes.value * 60;
+    }
+
+    function minutesConvert(params) {
+        hours.value = minutes.value / 60;
+        days.value = hours.value / 24;
+        seconds.value = minutes.value * 60;
+    }
+
+    function secondsConvert(params) {
+        minutes.value = seconds.value / 60;
+        hours.value = minutes.value / 60;
+        days.value = hours.value / 24;
+    }
 }
