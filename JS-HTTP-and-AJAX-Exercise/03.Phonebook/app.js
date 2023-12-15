e87675dedba20765f5d9e546863f483d91e55b76
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-15 20:01:52 +0200

    JS-HTTP-and-AJAX-Exercise

--- JS-HTTP-and-AJAX-Exercise/03.Phonebook/app.js
+++ JS-HTTP-and-AJAX-Exercise/03.Phonebook/app.js
@@ -1,5 +1,56 @@
 function attachEvents() {
-    // TODO:
+    document.getElementById('btnLoad').addEventListener('click', getAllPhones);
+    document.getElementById('btnCreate').addEventListener('click', createContact);
+    const baseUrl = 'http://localhost:3030/jsonstore/phonebook';
+
+    async function getAllPhones() {
+        const phonesInfo = await (await fetch(baseUrl)).json();
+
+        const phoneBook = document.getElementById('phonebook');
+
+        Object.values(phonesInfo).forEach((contact) => {
+            const li = document.createElement('li');
+
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+
+            li.textContent = `${contact.person}: ${contact.phone}`;
+            li.appendChild(deleteButton);
+            phoneBook.appendChild(li);
+
+            deleteButton.addEventListener('click', deleteContact);
+
+            function deleteContact() {
+                const id = contact._id;
+
+                fetch(`http://localhost:3030/jsonstore/phonebook/${id}`,
+                    {
+                        method: 'DELETE',
+                    });
+                li.remove();
+            }
+        });
+    }
+
+    function createContact() {
+        const person = document.getElementById('person').value;
+        const phone = document.getElementById('phone').value;
+
+        fetch(baseUrl, {
+            method: 'POST',
+            body: JSON.stringify(
+                {
+                    person,
+                    phone
+                }
+            )
+        }).then((response) => response.json())
+            .then(() => {
+                document.getElementById('person').value = '';
+                document.getElementById('phone').value = '';
+            });
+
+    }
 }
 
 attachEvents();
